1cd3b21aaff27713e364043093833daa67320f7a
wkrzyzanowski <wiktor.krzyzanowski1@gmail.com>
2021-04-07 22:18:59 +0200

    Added files for Kafka cluster K8s deployment.

--- src/main/java/pl/wiktor/kafkaapi/controller/MessageController.java
+++ src/main/java/pl/wiktor/kafkaapi/controller/MessageController.java
@@ -1,11 +1,13 @@
 package pl.wiktor.kafkaapi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.wiktor.kafkaapi.services.KafkaProducer;
 
+@Slf4j
 @RestController
 @RequestMapping("/msg")
 public class MessageController {
@@ -18,6 +20,7 @@ public class MessageController {
 
     @PostMapping
     public String createMessage(@RequestBody() String message) {
+        log.info("Message publish request: " + message);
         return kafkaProducer.publish(message);
     }
 }
